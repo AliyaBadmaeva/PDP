554a568f7b28a14d0b5ec2df71dbf6f01a23e3d0
AliyaBadmaeva <aliushik.azatovna@gmail.com>
2025-08-21 21:04:30 +0300

    Добавила суперпользователя, пользователя (студента), страницу входа для пользователя в html, добавила в urls.py

--- itos/itos/itos/urls.py
+++ itos/itos/itos/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+admin.site.site_header = "ITOS Admin"
+admin.site.site_title = "ITOS Admin Portal"
+admin.site.index_title = "Welcome to ITOS Portal"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('login/',  auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- itos/itos/main/views.py
+++ itos/itos/main/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Получение данных из БД
+def index(request):
+    return render(request, 'index.html')
