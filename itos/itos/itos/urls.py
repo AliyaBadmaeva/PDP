3e07dd5f840220c03aaa966725141b7fe7b258bd
AliyaBadmaeva <aliushik.azatovna@gmail.com>
2025-08-22 08:09:44 +0300

    Переход (редирект) со страницы входа на главную

--- itos/itos/itos/urls.py
+++ itos/itos/itos/urls.py
@@ -1,26 +1,12 @@
-"""
-URL configuration for itos project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
-admin.site.site_header = "ITOS Admin"
-admin.site.site_title = "ITOS Admin Portal"
-admin.site.index_title = "Welcome to ITOS Portal"
+
+admin.site.site_header = "Администратор ИТОС"
+admin.site.site_title = "Портал администратора ИТОС"
+admin.site.index_title = "Добро пожаловать в ИТОС!"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- itos/itos/main/views.py
+++ itos/itos/main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Получение данных из БД
 def index(request):
     return render(request, 'index.html')
+
+@login_required
+def main(request):
+    return render(request, 'main.html')
