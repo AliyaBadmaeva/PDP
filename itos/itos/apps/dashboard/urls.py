a260bc166c9211b307d9527bc8c7045854e10a31
AliyaBadmaeva <164990042+AliyaBadmaeva@users.noreply.github.com>
2025-09-14 14:50:14 +0300

    Добавление экспорта отзывов в urls.py

--- itos/itos/apps/dashboard/urls.py
+++ itos/itos/apps/dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import export_reviews_to_excel
 
 app_name = 'dashboard'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('manager/',   views.ManagerDashboardView.as_view(), name='manager'),
     path('student/',   views.StudentDashboardView.as_view(), name='student'),
     path('teacher/',   views.TeacherDashboardView.as_view(), name='teacher'),
+    path('export/reviews/', export_reviews_to_excel, name='export_reviews'),
 ]
