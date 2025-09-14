63bdc22357c72afac165d62d63a714f8081c3045
AliyaBadmaeva <164990042+AliyaBadmaeva@users.noreply.github.com>
2025-09-14 14:43:59 +0300

    Обновление admin.py

--- itos/itos/apps/accounts/admin.py
+++ itos/itos/apps/accounts/admin.py
@@ -4,7 +4,6 @@ from .models import StudentGroup, Profile
 
 
 
-admin.site.register(Review)
 admin.site.register(User)
 admin.site.register(LearningSubject)
 admin.site.register(Subject)
@@ -12,3 +11,9 @@ admin.site.register(Curriculum)
 admin.site.register(Student)
 admin.site.register(StudentGroup)
 admin.site.register(Profile)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id_review', 'user', 'learning_subject',
+                    'date_of_loading', 'uploaded_by')
