ba0c782e12b9c4216d9d51916e314a740e1bafe7
AliyaBadmaeva <164990042+AliyaBadmaeva@users.noreply.github.com>
2025-09-19 10:39:42 +0300

    Update admin.py

--- itos/itos/apps/accounts/admin.py
+++ itos/itos/apps/accounts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Review, User, LearningSubject, Subject, Curriculum, Student
 from .models import StudentGroup, Profile
-
-
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
 admin.site.register(User)
 admin.site.register(LearningSubject)
@@ -11,6 +11,8 @@ admin.site.register(Curriculum)
 admin.site.register(Student)
 admin.site.register(StudentGroup)
 admin.site.register(Profile)
+
+
 
 
 @admin.register(Review)
