860296efa79794d940aa90ca90fbcf9beb4feee8
AliyaBadmaeva <164990042+AliyaBadmaeva@users.noreply.github.com>
2025-09-19 13:13:11 +0300

    Update urls.py

--- itos/itos/itos/urls.py
+++ itos/itos/itos/urls.py
@@ -1,17 +1,24 @@
+# Корневой URL-конфигуратор проекта itos.
+# Подключает админку, приложения и задаёт заголовки админ-интерфейса.
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
-admin.site.site_header = "Администратор ИТОС"
-admin.site.site_title = "Портал администратора ИТОС"
-admin.site.index_title = "Добро пожаловать в ИТОС!"
+# Заголовки в админке
+admin.site.site_header = "Администратор ИТОС"  # верхняя строка
+admin.site.site_title = "Портал администратора ИТОС"  # <title>
+admin.site.index_title = "Добро пожаловать в ИТОС!"  # заголовок на главной
 
 urlpatterns = [
+    # заголовок на главной
     path('admin/', admin.site.urls),  # админка Django
+    # Корень сайта - сразу переводим на блог
     path('', RedirectView.as_view(url='/blog/', permanent=False)),
-    path('blog/', include('blog.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('dashboard/', include('dashboard.urls')),
+    # Подключаем URL-ы приложений
+    path('blog/', include('blog.urls')),  # информационные страницы
+    path('accounts/', include('accounts.urls')),  # вход / выход / редирект по ролям
+    path('dashboard/', include('dashboard.urls')),  # личные кабинеты (student, teacher, manager)
 ]
 
 
