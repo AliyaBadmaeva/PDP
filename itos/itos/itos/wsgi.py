62ffc0e19e362088e0d6a46ec3fcd72e5fcb3bd9
AliyaBadmaeva <164990042+AliyaBadmaeva@users.noreply.github.com>
2025-09-19 13:13:22 +0300

    Update wsgi.py

--- itos/itos/itos/wsgi.py
+++ itos/itos/itos/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for itos project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
+# WSGI-конфигурация для проекта itos.
+# Используется продакшн-сервером (gunicorn, uWSGI, mod_wsgi) для запуска Django-приложения.
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Указываем Django, где находится модуль настроек (переменная окружения обычно задаётся в systemd/docker)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "itos.settings")
 
+# Создаём WSGI-приложение, которое будет обрабатывать HTTP-запросы
 application = get_wsgi_application()
