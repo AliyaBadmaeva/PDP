55bc9068f1f9c0d5b5feed3c1c2328685f46901a
AliyaBadmaeva <164990042+AliyaBadmaeva@users.noreply.github.com>
2025-09-19 12:19:15 +0300

    Комментарии к urls.py

--- itos/itos/apps/blog/urls.py
+++ itos/itos/apps/blog/urls.py
@@ -1,14 +1,17 @@
+# URL-маршруты приложения blog (информационные страницы сайта)
 from django.urls import path
 from . import views
 
+# Пространство имён позволяет обращаться к url-ам через
+# {% url 'blog:main' %}, {% url 'blog:faq' %} и т.д.
 app_name = 'blog'
 
 
 urlpatterns = [
-    path('',          views.MainView.as_view(),   name='main'),
-    path('faq/',      views.FaqView.as_view(),   name='faq'),
-    path('about/',    views.about_page, name='about'),
-    path('contacts/',  views.ContactView.as_view(), name='contacts'),
-    path('nlp/', views.NlpView.as_view(), name='nlp'),
-    path('eda/', views.EdaView.as_view(), name='eda'),
+    path('',          views.MainView.as_view(),   name='main'),  # Главная страница
+    path('faq/',      views.FaqView.as_view(),   name='faq'),  # FAQ
+    path('about/',    views.about_page, name='about'),  # «О программе» – реализована через функцию-представление
+    path('contacts/',  views.ContactView.as_view(), name='contacts'),  # Контакты
+    path('nlp/', views.NlpView.as_view(), name='nlp'),  # Страница «Технологии NLP»
+    path('eda/', views.EdaView.as_view(), name='eda'),  # Страница «Разведочный анализ данных (EDA)»
 ]
