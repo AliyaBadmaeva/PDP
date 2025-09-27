8059ce05573825276f11722cf140a948a5676620
AliyaBadmaeva <164990042+AliyaBadmaeva@users.noreply.github.com>
2025-09-27 07:53:40 +0300

    Обновление forms.py

--- itos/itos/apps/accounts/forms.py
+++ itos/itos/apps/accounts/forms.py
@@ -1,1 +1,5 @@
 from django import forms
+
+class LoginForm(forms.Form):  # Форма авторизации
+    username = forms.CharField(label='Логин', max_length=100)
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
